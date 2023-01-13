fb18fe8d8f2941fcc122cc96a59a2fa59a3260c9
yakzanCode <a.yakzan@outlook.de>
2023-01-14 00:48:37 +0100

    1st commit

---
+++ T1.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+const PORT = process.env.PORT || 9009;
+
+app.use(cors())
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/data', function (req, res) {
+    var data = [
+        {id:17,name:"Ahmad"},
+        {id:17,name:"Ahmad"},
+        {id:17,name:"Ahmad"}
+    ]
+    res.send(data)
+  })
+app.listen(PORT)
